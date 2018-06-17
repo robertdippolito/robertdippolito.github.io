78a244ef0fcdf4c617e6a311b4d62a6947150a7c
robertdippolito <robert.e.dippolito@gmail.com>
2018-06-17 12:51:24 -0400

    added template for books - next comes content generation and add the v-for statement

--- src/components/ReadingList.vue
+++ src/components/ReadingList.vue
@@ -9,17 +9,25 @@
       </router-link>
     </div>
     <div class="hero-body">
-      <div class="columns is-8">
-        <div class="column">
-          First column
+      <div class="columns">
+        <div class="column is-one-third">
+          <div class="tile is-parent">
+            <article class="tile is-child" style="justify-content:center;">
+              <figure style="text-align:center;">
+                <img src="https://i.imgur.com/F7kY1LV.jpg" style="width:50%;height:50%;">
+              </figure>
+            </article>
+          </div>
         </div>
-        <div class="column">
+        <div class="column is-two-thirds">
           <div class="content">
-            <h1>Hello World</h1>
+            <h1 class="title">Lean In</h1>
+            <h2 class="subtitle is-size-4">Sheryl Sandberg</h2>
             <p>Lorem ipsum<sup><a>[1]</a></sup> dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque. Sub<sub>script</sub> works as well!</p>
           </div>
         </div>
       </div>
+      <hr/>
     </div>
   </section>
 </template>
