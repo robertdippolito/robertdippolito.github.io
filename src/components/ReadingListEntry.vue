216e25d6ec96890d27decca424226918c0ced5a3
robertdippolito <robert.e.dippolito@gmail.com>
2018-06-19 13:08:22 -0400

    added content for the reviews I have done to date.

--- src/components/ReadingListEntry.vue
+++ src/components/ReadingListEntry.vue
@@ -5,7 +5,7 @@
         <div class="tile is-parent">
           <article class="tile is-child" style="justify-content:center;">
             <figure style="text-align:center;">
-              <img v-bind:src="entry.imageURL" style="width:50%;height:50%;">
+              <img v-bind:src="entry.imageURL" style="width:50%;height:50%; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
             </figure>
           </article>
         </div>
--- src/readingListData.js
+++ src/readingListData.js
@@ -1,16 +1,44 @@
 export const readingListData = [
   {
     id: 1,
+    title: 'Inner Excellence',
+    author: 'Jim Murphy',
+    imageURL: 'https://i.imgur.com/UYzMN1F.jpg',
+    body: "Currently Reading :)"
+  },
+  {
+    id: 1,
     title: 'Lean In',
     author: 'Sheryl Sandberg',
     imageURL: 'https://i.imgur.com/F7kY1LV.jpg',
-    body: 'Body text...'
+    body: "Review Coming Soon.."
   },
   {
     id: 2,
+    title: 'Hit Refresh',
+    author: 'Satya Nadella',
+    imageURL: 'https://i.imgur.com/ChyRHFo.jpg',
+    body: "Satya, the current CEO of Microsoft explores the challenges of stepping into the driver seat of one of the largest tech companies in the world. Not a chief, Satya prefers to refer to himself as the executive culture officer, a very fitting title for the theme of the book. Click Refresh dives into the challenges that exist when a company as successful as Microsoft is forced to expand into new frontiers. For years Microsoft has dominated the PC market, however, after missing out on the explosion of mobile, many had suspected that Microsoft was reaching maturity. With a strategy in place and a strong promise to become major players in the Cloud and AI space, Satya set out to change the culture of Microsoft to embrace the change. Hit Refresh book felt like an address to employees more then it felt like a book for the general public. Satya explains how he plans to empower employees and reflects on some of the more public litigations since he has taken the helm. I appreciate these reflections because I feel like it gives a great insight into what is really happening behind the scenes. Unfortunately, it sounded like it was heavily reviewed by their PR team taking away a lot of the authenticity. I enjoyed reading about Satya’s rise through the company but would have liked to hear more. Overall, Hit Refresh provides a lens inside one of the biggest tech companies in the world which I found to be incredibly interesting. However, I feel like the lens that lets us inside didn't come off as authentic and left a lot to be desired."
+  },
+  {
+    id: 3,
+    title: 'Better Faster',
+    author: 'Jeremy Gutsche',
+    imageURL: 'https://i.imgur.com/RRiOGBM.jpg',
+    body: "Better and Faster by Jeremy Gutsche focuses on describing the pitfalls that businesses make when sticking to their farmer roots and not embracing their inner hunter. The book focused on 6 patterns: convergence, divergence, circularity, reduction, acceleration and redirection and how companies have successfully embraced these patterns and reaped the benefits. The book covers different case studies across many different industries ranging from banking, the founder of Zara to his own successful entrepreneurial family. My favourite parts of the book were when he talked about his own experiences with entrepreneurship. They felt genuine and his personal story is very relatable to many young professionals. At times I felt that the farmer / hunter analogy seemed forced and I noticed some common themes emerged across all of the patterns (i.e. embrace social, go digital). Jeremy has built and scaled an agency that is truly impressive and I am excited to see what future has in store for Trend Hunter."
+  },
+  {
+    id: 4,
+    title: 'Rich Dad Poor Dad',
+    author: 'Robert T. Kiyosaki',
+    imageURL: 'https://i.imgur.com/PyxK9Hl.jpg',
+    body: "Rich Dad Poor Dad (RDPD) reveals the secrets of the wealthy and the lessons learned from Robert’s biological father (poor dad) and his friend Mike’s father (rich dad). These lessons, over the course of his life, have shaped him into the successful business man that he is today - often comparing approaches between his two father figures. His poor dad, a university professor and government man fell into the upper middle class, however, with little financial intelligence his poor dad continuously lived pay cheque to pay cheque. Alternatively, his rich dad, an entrepreneur who owned several businesses went on to become one of the richest men in Hawaii because he was able to successfully apply his financial intelligence throughout his life. The book is broken out into 6 lessons (the rich don’t work for money, why teach financial literacy, mind your own business, the history of taxes and the power of corporations, the rich invest money, work to learn) that are explained in depth throughout each chapter. I found many of the lessons to be very impactful and perspective changing. Especially the simple ideas like investing in true assets not liabilities. Robert did a good job of explaining each of the lessons, however, his reflection on some of the lessons felt shallow and biased towards his rich dad’s views. The beginning of the book states that both of his fathers views have shaped him into the person he is today but throughout the book his poor dad is consistently used as an example of what not to do. The book aggressively challenges the readers views and forces a middle ground. Overall, the book was thought provoking and challenged my beliefs when it came to financial intelligence. Robert’s lessons stuck with me and I found myself reflecting on them frequently with colleagues at work."
+  },
+  {
+    id: 5,
     title: 'Resilience',
-    author: 'First Name',
-    imageURL: 'https://i.imgur.com/fR9JwaV.jpg',
-    body: 'Body text...'
+    author: 'Eric Greitens',
+    imageURL: 'https://i.imgur.com/CwnhOjB.jpg',
+    body: "Review Coming Soon.."
   }
 ]
