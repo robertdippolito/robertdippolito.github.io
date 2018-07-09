3b945ec3a7a399204c3c962866b137cd0ff6d429
robertdippolito <robert.e.dippolito@gmail.com>
2018-07-09 14:58:54 -0400

    added the new reviews. TODO: add experience and work sections (similar forms to reading list). Removed the Articles because it is redundant with a Medium link.

--- src/readingListData.js
+++ src/readingListData.js
@@ -1,41 +1,48 @@
 export const readingListData = [
   {
     id: 1,
+    title: 'Adults in the Room',
+    author: 'Yanis Varoufakis',
+    imageURL: 'https://i.imgur.com/OZzeoUZ.jpg',
+    body: "Currently Reading"
+    },
+  {
+    id: 2,
     title: 'Inner Excellence',
     author: 'Jim Murphy',
     imageURL: 'https://i.imgur.com/UYzMN1F.jpg',
-    body: "Currently Reading :)"
+    body: "Inner Excellence is a self-help novel written by former pro baseball player. The book defines tools and strategies that one can employ to overcome self constructed barriers and achieve peak performance. In my experience, the success of this type of book comes from the authors ability to relate the concepts to examples. Jim has done a good job with this drawing on examples from some of the greats like Michael Jordan and (coach of the buck-eyes). I did feel as though there was a lot of overlap with the concepts, which I think happens when you try to stretch a framework out to fit into a full length novel. Some of the key takeaways for me were: Be present in the moment, Practice self-affirmation daily, When building relationships, pace then lead, and self-mastery is the key to peak performance. There were many other concepts that were prevalent throughout the book such as the trickster, monkey mind and the critic constantly challenging our perspective and the idea of the affluenza virus and ego clouding our path to self-mastery. Overall, I thought the frameworks defined in the book were powerful and I will definitely take some of them away and apply them in my life, however, I think a lot of the book felt like filler and could’ve been captured in a much more condensed version."
   },
   {
-    id: 1,
+    id: 3,
     title: 'Lean In',
     author: 'Sheryl Sandberg',
     imageURL: 'https://i.imgur.com/F7kY1LV.jpg',
-    body: "Review Coming Soon.."
+    body: "Lean in is Sheryl Sandberg’s first novel that discusses the gender inequalities that exist in today’s society. As a young male business and technology professional a lot of what Sheryl described resonated strongly with me. I’ve always acknowledged the facts surrounding gender inequality (i.e. pay gaps and career progression) but I never understood the implications. Sheryl does a fantastic job of not only highlighting the issues but using her personal experiences to contextualize the challenges facing women making them much more meaningful. Lean In also offers tips and guidance for young women on the best ways to overcome these challenges and I think its a must read for any new grad."
   },
   {
-    id: 2,
+    id: 4,
     title: 'Hit Refresh',
     author: 'Satya Nadella',
     imageURL: 'https://i.imgur.com/ChyRHFo.jpg',
     body: "Satya, the current CEO of Microsoft explores the challenges of stepping into the driver seat of one of the largest tech companies in the world. Not a chief, Satya prefers to refer to himself as the executive culture officer, a very fitting title for the theme of the book. Click Refresh dives into the challenges that exist when a company as successful as Microsoft is forced to expand into new frontiers. For years Microsoft has dominated the PC market, however, after missing out on the explosion of mobile, many had suspected that Microsoft was reaching maturity. With a strategy in place and a strong promise to become major players in the Cloud and AI space, Satya set out to change the culture of Microsoft to embrace the change. Hit Refresh book felt like an address to employees more then it felt like a book for the general public. Satya explains how he plans to empower employees and reflects on some of the more public litigations since he has taken the helm. I appreciate these reflections because I feel like it gives a great insight into what is really happening behind the scenes. Unfortunately, it sounded like it was heavily reviewed by their PR team taking away a lot of the authenticity. I enjoyed reading about Satya’s rise through the company but would have liked to hear more. Overall, Hit Refresh provides a lens inside one of the biggest tech companies in the world which I found to be incredibly interesting. However, I feel like the lens that lets us inside didn't come off as authentic and left a lot to be desired."
   },
   {
-    id: 3,
+    id: 5,
     title: 'Better Faster',
     author: 'Jeremy Gutsche',
     imageURL: 'https://i.imgur.com/RRiOGBM.jpg',
     body: "Better and Faster by Jeremy Gutsche focuses on describing the pitfalls that businesses make when sticking to their farmer roots and not embracing their inner hunter. The book focused on 6 patterns: convergence, divergence, circularity, reduction, acceleration and redirection and how companies have successfully embraced these patterns and reaped the benefits. The book covers different case studies across many different industries ranging from banking, the founder of Zara to his own successful entrepreneurial family. My favourite parts of the book were when he talked about his own experiences with entrepreneurship. They felt genuine and his personal story is very relatable to many young professionals. At times I felt that the farmer / hunter analogy seemed forced and I noticed some common themes emerged across all of the patterns (i.e. embrace social, go digital). Jeremy has built and scaled an agency that is truly impressive and I am excited to see what future has in store for Trend Hunter."
   },
   {
-    id: 4,
+    id: 6,
     title: 'Rich Dad Poor Dad',
     author: 'Robert T. Kiyosaki',
     imageURL: 'https://i.imgur.com/PyxK9Hl.jpg',
     body: "Rich Dad Poor Dad (RDPD) reveals the secrets of the wealthy and the lessons learned from Robert’s biological father (poor dad) and his friend Mike’s father (rich dad). These lessons, over the course of his life, have shaped him into the successful business man that he is today - often comparing approaches between his two father figures. His poor dad, a university professor and government man fell into the upper middle class, however, with little financial intelligence his poor dad continuously lived pay cheque to pay cheque. Alternatively, his rich dad, an entrepreneur who owned several businesses went on to become one of the richest men in Hawaii because he was able to successfully apply his financial intelligence throughout his life. The book is broken out into 6 lessons (the rich don’t work for money, why teach financial literacy, mind your own business, the history of taxes and the power of corporations, the rich invest money, work to learn) that are explained in depth throughout each chapter. I found many of the lessons to be very impactful and perspective changing. Especially the simple ideas like investing in true assets not liabilities. Robert did a good job of explaining each of the lessons, however, his reflection on some of the lessons felt shallow and biased towards his rich dad’s views. The beginning of the book states that both of his fathers views have shaped him into the person he is today but throughout the book his poor dad is consistently used as an example of what not to do. The book aggressively challenges the readers views and forces a middle ground. Overall, the book was thought provoking and challenged my beliefs when it came to financial intelligence. Robert’s lessons stuck with me and I found myself reflecting on them frequently with colleagues at work."
   },
   {
-    id: 5,
+    id: 7,
     title: 'Resilience',
     author: 'Eric Greitens',
     imageURL: 'https://i.imgur.com/CwnhOjB.jpg',
