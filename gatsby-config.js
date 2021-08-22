5160dafec9a6720713da8878de21a01368d13664
r.dippolito <robert.e.dippolito@accenture.com>
2021-08-22 10:12:23 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Rob's Cloud Computing Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Robert D'Ippolito`,
+      summary: `who writes about all things AWS.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
   },
   plugins: [
     `gatsby-plugin-image`,
