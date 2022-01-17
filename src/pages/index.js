ed2001f7ae580cd0966924ed0487c73091f8009d
Robert D'Ippolito <robertdippolito@MacBook-Pro.local>
2022-01-17 13:08:37 -0500

    fixed blog post layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import '../style.css'
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
@@ -39,15 +40,16 @@ const BlogIndex = ({ data, location }) => {
                 itemType="http://schema.org/Article"
               >
                 <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
+                  <div>
+                    <Link className="blog-main-post-link" to={post.fields.slug} itemProp="url">
+                      <span className="blog-main-post-title" itemProp="headline">{title}</span>
                     </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
+                  </div>
+                  <small className="blog-main-post-date">{post.frontmatter.date}</small>
                 </header>
                 <section>
-                  <p
+                  <div
+                    className="blog-main-post-description"
                     dangerouslySetInnerHTML={{
                       __html: post.frontmatter.description || post.excerpt,
                     }}
