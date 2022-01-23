313f6a0f7bfbff11b932aad47504aae2a864079d
Robert D'Ippolito <robertdippolito@MacBook-Pro.local>
2022-01-22 22:22:54 -0500

    added the styling for the error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,14 +4,18 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import "../style/error.css"
+
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className="error-container">
+        <div className="error-title">404: Sorry, the page you're looking for isn't here</div>
+        <div className="error-body">You just hit a route that doesn&#39;t exist... the sadness.</div>
+      </div>
     </Layout>
   )
 }
